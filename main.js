7d6e60985ab885de7fb60f29bdd62d5e786ad296
pratushrai0309 <pratushrai0309@gmail.com>
2020-11-03 16:29:16 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+  data: () => {
+    return {
+      product: 'Socks',
+      img: './assets/images/socks_blue.jpg',
+      url: 'https://github.com/pratushrai0309'
+    }
+  }
+})
